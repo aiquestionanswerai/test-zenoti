b43edd76eeee4d85998a172ccb3da6a1cb7d7e68
alvinalmoite <ai.alvinalmoite@gmail.com>
2026-06-27 15:21:24 +0800

    add pw browser

--- miner.py
+++ miner.py
@@ -10,6 +10,10 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
+browsers_path = os.getenv("PLAYWRIGHT_BROWSERS_PATH")
+if browsers_path:
+    os.environ["PLAYWRIGHT_BROWSERS_PATH"] = browsers_path
+    
 USERNAME = os.getenv("MINER_USER")
 PASSWORD = os.getenv("MINER_PASSWORD")
 ADMIN_URL = "https://evolvemedspa.zenoti.com/Admin/Admin.aspx"
